99310bb5a2fe8d3a5b9f9d0fcaf726fe3fd491fe
neeleshgarg <62295007+neeleshgarg@users.noreply.github.com>
2020-07-23 19:48:17 +0530

    update

--- popup.js
+++ popup.js
@@ -1,18 +1,19 @@
+let checkbox = document.getElementById('DisableCSP');
 function myFunction() {
   console.log('fired');
-  
-    if (checkbox.checked) {
-      chrome.runtime.sendMessage({ CSPstate: "check" });
-    }
-    else {
-      console.log("NO");
-    }
+  if (checkbox.checked) {
+    chrome.runtime.sendMessage({ CSPstate: "check" });
+  }
+  else {
+    console.log("NO");
+  }
 }
-
-
 window.onload = function () {
-  document.getElementById('DisableCSP').addEventListener('change', myFunction);
-}
-
-
-let checkbox = document.getElementById('DisableCSP');
+  document.getElementById('DisableCSP').addEventListener('change', myFunction);  
+  chrome.runtime.onMessage.addListener(function (req, send, sendRes) {
+    console.log('message check');
+    if (req.state === "checked") {
+      document.getElementById('DisableCSP').checked=true;
+    }
+  });
+};
