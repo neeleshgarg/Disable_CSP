28d3ff79be16dc5f287c8296292957efb5a4cc37
neeleshgarg <62295007+neeleshgarg@users.noreply.github.com>
2020-07-27 16:45:30 +0530

    update

--- popup.js
+++ popup.js
@@ -4,9 +4,6 @@ function myFunction() {
   if (checkbox.checked) {
     chrome.runtime.sendMessage({ CSPstate: "check" });
   }
-  else {
-    console.log("NO");
-  }
 }
 window.onload = function () {
   document.getElementById('DisableCSP').addEventListener('change', myFunction);  
